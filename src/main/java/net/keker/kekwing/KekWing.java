38c96cc90a26ab0c24f776b76dfedd2bd1407383
jorge <jorgeeduardodsc@gmail.com>
2023-08-09 17:20:59 +0100

    first commit

--- src/main/java/net/keker/kekwing/KekWing.java
+++ src/main/java/net/keker/kekwing/KekWing.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-//
+// kek
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(KekWing.MOD_ID)
 public class KekWing
